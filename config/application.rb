c110af01fe331bcf62ccc884c267bf1d4703602a
Shinsuke Nishio <nishio@densan-labs.net>
2020-01-18 18:01:31 +0900

    No commit messages.

---
+++ config/application.rb
@@ -0,0 +1,8 @@
+require_relative "boot"
+
+require "tiny_rails"
+
+module SampleApp
+  class Application < TinyRails::Application
+  end
+end
---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Tiny Rails application.
+require_relative "application"
+
+# Initialize the Rails application.
+TinyRails.application.initialize!
